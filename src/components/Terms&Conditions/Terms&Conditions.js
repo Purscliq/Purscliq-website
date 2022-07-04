fa53031dfcc6b0cc6bceca6a860debfa06823d02
Israel David <77063876+israel1281@users.noreply.github.com>
2022-07-04 01:48:12 -0700

    created routes for terms-conditions

---
+++ src/components/Terms&Conditions/Terms&Conditions.js
@@ -0,0 +1,11 @@
+import Header2 from "../header/Header2";
+import Footer from "../footer/footer";
+
+export default function Terms() {
+  return (
+    <div className="App">
+      <Header2 />
+      <Footer />
+    </div>
+  );
+}
